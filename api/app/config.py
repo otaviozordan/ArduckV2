7357c497593ee8aada1f17c3965d0484728bc34a
Otávio Zordan Alves <otavio.zordan@outlook.com>
2023-09-08 14:53:50 -0300

    .

--- api/app/config.py
+++ api/app/config.py
@@ -2,7 +2,7 @@
 
 DEBUG = True
 SECRET_KEY = 'sua_chave_secreta'
-MONGO_URI  = 'mongodb://localhost:27017/'
+MONGO_URI  = 'mongodb://mongo:27017/'
 
 DATABASE_IMG_PATH = 'api\\app\\models\\imgs\\'
 
