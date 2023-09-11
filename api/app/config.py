d67c2bfe8371134e77e708fccbc918bd5727eaf0
Otávio Zordan Alves <otavio.zordan@outlook.com>
2023-09-11 12:48:58 -0300

    .

--- api/app/config.py
+++ api/app/config.py
@@ -2,7 +2,8 @@
 
 DEBUG = True
 SECRET_KEY = 'sua_chave_secreta'
-MONGO_URI  = 'mongodb://mongo:27017/'
+#MONGO_URI  = 'mongodb://mongo:27017/'
+MONGODB_URI = 'mongodb://debugduck:duckduckgo@mongodb:27017/'
 
 DATABASE_IMG_PATH = 'api\\app\\models\\imgs\\'
 
