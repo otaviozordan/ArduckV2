32c41d1d2482c6568f5bdc718c9b14ecc054b64f
Otávio Zordan Alves <otavio.zordan@outlook.com>
2023-10-04 21:41:21 -0300

    AGORA VAI CARALHO

--- api/app/config.py
+++ api/app/config.py
@@ -3,8 +3,8 @@
 DEBUG = True
 SECRET_KEY = 'sua_chave_secreta'
 #MONGO_URI  = 'mongodb://mongo:27017/'
-MONGO_URI  = 'mongodb://localhost:27017/'
+#MONGO_URI  = 'mongodb://localhost:27017/'
 #MONGO_URI = 'mongodb://debugduck:duckduckgo@localhost:27017/'
-#MONGO_URI = 'mongodb://debugduck:duckduckgo@mongodb:27017/'
+MONGO_URI = 'mongodb://debugduck:duckduckgo@mongodb:27017/'
 
 UPLOAD_FOLDER = 'api\\app\\static\\imgs'
